767509f87cbefe08d022d8ffbe544add895aea00
coolwuu <coolwuu0207@gmail.com>
2017-09-01 22:47:18 +0800

    pass 3rd test

--- TwoFer/Twofer.cs
+++ TwoFer/Twofer.cs
@@ -6,6 +6,8 @@
         {
             if(input == "Alice")
                 return "One for Alice, one for me.";
+            if (input == "Bob")
+                return "One for Bob, one for me.";
             return "One for you, one for me.";
         }
     }
