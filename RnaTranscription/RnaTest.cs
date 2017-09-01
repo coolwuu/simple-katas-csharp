8a38b10e5d9bee39549c97e1b9452907fa80fb01
coolwuu <coolwuu0207@gmail.com>
2017-09-01 22:30:59 +0800

    解決一開始沒辦法跑Test

--- RnaTranscription/RnaTest.cs
+++ RnaTranscription/RnaTest.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace RnaTranscription
 {
+    [TestFixture]
     public class RnaTest
     {
         [Test]
