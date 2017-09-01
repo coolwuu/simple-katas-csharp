8c2e53ffd32cae34b48f13d0b49a44e4a09a3e98
coolwuu <coolwuu0207@gmail.com>
2017-09-02 00:10:50 +0800

    pass test

--- StringAverage/StringAverageCalculator.cs
+++ StringAverage/StringAverageCalculator.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StringAverage
 {
     class StringAverageCalculator
     {
+        public static Dictionary<string, int> NumberMapping = new Dictionary<string, int>()
+        {
+            {"zero",0 },{"one",1 },{"two",2 },{"three",3},{"four",4 },
+            {"five",5},{"six",6 },{"seven",7 },{"eight",8 },{"nine",9 },
+        };
+
         public static string Calculate(string input)
         {
             if (String.IsNullOrEmpty(input))
                 return "n/a";
-            return input;
+
+            var numberList = input.Split(' ');
+            var total = 0;
+            foreach (var number in numberList)
+            {
+                if (!NumberMapping.ContainsKey(number))
+                    return "n/a";
+                var tmp = NumberMapping[number];
+                total += tmp;
+            }
+            int avgResult = total / numberList.Length;
+
+            return NumberMapping.FirstOrDefault(x => x.Value == avgResult).Key;
         }
     }
+
+
+    
+    
 }
